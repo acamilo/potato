690fcf66bcc6cba0eb860873cc76ae35ec61591b
Alex Camilo <alex.camilo@gmail.com>
2023-01-15 16:30:56 -0500

    potatoes dont have struts

--- potato.groovy
+++ potato.groovy
@@ -1,1 +1,22 @@
-// code here
+@groovy.transform.InheritConstructors
+class Tank {
+	int facets = 16;
+	double length = 600;
+	double diameter = 150;
+	double cyllen = length-diameter;
+	def toCSG(){
+		CSG sphere = new Sphere(diameter,facets,8).toCSG().rotx(90);
+		CSG body = new Cylinder(	diameter, // Radius at the top
+		diameter, // Radius at the bottom
+		cyllen, // Height
+		(int)facets //resolution
+		).toCSG();
+		body = body.union([sphere,sphere.movez(cyllen)]);
+		body = body.movez(-cyllen/2)
+		body = body.rotz(22.5);
+		
+		return body;
+	}
+}
+
+return new Tank().toCSG();
